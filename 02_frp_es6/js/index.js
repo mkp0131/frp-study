93d18823d4dd02fda59793e764b7e3e224879b52
mkpMac <mkp0131@naver.com>
2020-08-22 22:40:38 +0900

    20200822

--- 02_frp_es6/js/index.js
+++ 02_frp_es6/js/index.js
@@ -1,21 +1,36 @@
-// FRP ES6
+// # FRP ES6
 const products = [
-	{name: '반팔티', price: 15000},
-	{name: '긴팔티', price: 20000},
-	{name: '핸드폰케이스', price: 15000},
-	{name: '후드티', price: 30000},
-	{name: '바지', price: 25000}
+  { name: "반팔티", price: 15000 },
+  { name: "긴팔티", price: 20000 },
+  { name: "핸드폰케이스", price: 15000 },
+  { name: "후드티", price: 30000 },
+  { name: "바지", price: 25000 },
 ];
 
-// const set = new Set('121');
-// for (const iterator of set) {
-// 	console.log('', iterator);
-// }
-// console.log('', set);
+// ## map 을 직접 구현
+const map = (fn, iterable) => {
+  let result = [];
+  for (const iterator of iterable) {
+    result.push(fn(iterator));
+  }
+  return result;
+};
 
-const map = new Map();
-map.set(1,2);
-map.set('1');
-console.log('', map);
-map.clear();
-console.log('', map);
+// ### 자바스크립트 map 함수를 `arry like object` 에 사용하기
+// 1. bind 사용
+const _map = Array.prototype.map;
+const nodeList = document.querySelectorAll("*");
+// _map.call(nodeList, (element) => console.log("", element));
+
+// for (let i = 0; i < nodeList.length; i++) {
+//   const element = nodeList[i];
+//   console.log("", element);
+// }
+
+// const m = new Map();
+// m.set(1, 4);
+// m.set(2, 4);
+// m.set(3, 4);
+
+// const result = map(([key, value]) => [key, value * 2], m);
+// console.log("", result);
