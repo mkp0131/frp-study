142acc1bd5617d6c58c1c1158c23db22ad29f961
mkpMac <mkp0131@naver.com>
2020-08-24 01:24:29 +0900

    20200824

--- 02_frp_es6/js/index.js
+++ 02_frp_es6/js/index.js
@@ -1,10 +1,10 @@
 // # FRP ES6
 const products = [
-  { name: "반팔티", price: 15000 },
-  { name: "긴팔티", price: 20000 },
-  { name: "핸드폰케이스", price: 15000 },
-  { name: "후드티", price: 30000 },
-  { name: "바지", price: 25000 },
+  { name: "반팔티", price: 15000, quantity: 1, is_selected: true },
+  { name: "긴팔티", price: 20000, quantity: 2, is_selected: false },
+  { name: "핸드폰케이스", price: 15000, quantity: 3, is_selected: true },
+  { name: "후드티", price: 30000, quantity: 4, is_selected: false },
+  { name: "바지", price: 25000, quantity: 5, is_selected: true },
 ];
 
 // ## each [map, filter 에서 사용]
@@ -49,11 +49,7 @@ let reduce = (fn, acc, iterable) => {
 };
 
 // ## go (값을 즉시 평가)
-const go = (...fns) => {
-  reduce((acc, fn) => {
-    return fn(acc);
-  }, fns);
-};
+const go = (...fns) => reduce((acc, fn) => fn(acc), fns);
 
 // go(
 //   0,
@@ -90,6 +86,9 @@ const totalPirce = (predi) =>
     console.log
   );
 
+// ### add 함수
+const add = (a, b) => a + b;
+
 // go(
 //   products,
 //   totalPirce((item) => item.price > 20000)
@@ -99,3 +98,60 @@ const totalPirce = (predi) =>
 //   products,
 //   totalPirce((item) => item.price < 20000)
 // );
+
+// 연습문제
+// 1. 모든 item 의 수량을 합산해주는 함수
+const sum = curry((predi, products) => pipe(map(predi), reduce(add))(products));
+
+const total_price = sum((item) => item.quantity * item.price);
+
+const total_quantity = sum((item) => item.quantity);
+
+// result = total_quantity(products);
+// console.log("", result);
+
+const str = `<table>
+			<thead>
+				<tr>
+					<th></th>
+					<th>상품</th>
+					<th>가격</th>
+					<th>수량</th>
+					<th>합계</th>
+				</tr>
+			</thead>
+			<tbody>
+			${go(
+        products,
+        sum(
+          (item) =>
+            `<tr>
+						<td>
+							<input type="checkbox" ${item.is_selected ? "checked='checked'" : ""} />
+						</td>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+            <td>${item.quantity}</td>
+            <td>${item.quantity * item.price}</td>
+          </tr>`
+        )
+      )}
+			<tr>
+				<td colspan="2">합계</td>
+				<td>${total_quantity(
+          filter((item) => (item.is_selected ? item.quantity : 0), products)
+        )}
+				</td>
+				<td>${go(
+          products,
+          sum((item) => (item.is_selected ? item.quantity : 0))
+        )}</td>
+				<td>${go(
+          products,
+          sum((item) => (item.is_selected ? item.quantity * item.price : 0))
+        )}</td>
+			</tr>
+			</tbody>
+		</table>`;
+
+document.querySelector("#root").innerHTML = str;
